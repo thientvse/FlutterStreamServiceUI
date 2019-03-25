4b579e2fbcc3a58f17d5d5d3540372b76396175d
thientv7 <thientvse@gmail.com>
2019-03-25 17:00:53 +0700

    add button and background

--- flutter_stream_ui/lib/main.dart
+++ flutter_stream_ui/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'customicon.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -66,46 +68,156 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          children: <Widget>[HomeScreenTopPart()],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class HomeScreenTopPart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      height: 350,
+      child: Stack(
+        children: <Widget>[
+          ClipPath(
+            clipper: Mclipper(),
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0.0, 10.0),
+                    blurRadius: 10.0)
+              ]),
+              child: Stack(
+                children: <Widget>[
+                  Image.asset('assets/images/banner.png',
+                      fit: BoxFit.cover, width: double.infinity),
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                          const Color(0x00000000),
+                          const Color(0xD9000000),
+                        ],
+                            stops: [
+                          0.0,
+                          0.9
+                        ],
+                            begin: FractionalOffset(0.0, 0.0),
+                            end: FractionalOffset(0.0, 0.9))),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 120.0, left: 95.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "WATCH BEFORE EVERYONE",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                fontFamily: "SF-Pro-Display-Bold"),
+                          ),
+                          Text(
+                            "The Punisher: Season 2",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40.0,
+                                fontFamily: "SF-Pro-Display-Bold"),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 300.0,
+            right: -20.0,
+            child: FractionalTranslation(
+              translation: Offset(0.0, -0.5),
+              child: Row(
+                children: <Widget>[
+                  FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.add,
+                      color: Color(0xFFE52020),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12.0,
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: Color(0xFFE52020),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 80.0),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "Watch Now",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                                fontFamily: "SF-Pro-Display-Bold"),
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          RotatedBox(
+                            quarterTurns: 2,
+                            child: Icon(CustomIcons.back_icon,
+                                size: 25.0, color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Mclipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+    path.lineTo(0.0, size.height - 100.0);
+
+    var controlpoint = Offset(35.0, size.height);
+    var endpoint = Offset(size.width / 2, size.height);
+
+    path.quadraticBezierTo(
+        controlpoint.dx, controlpoint.dy, endpoint.dx, endpoint.dy);
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
